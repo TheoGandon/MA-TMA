9d3b19e3ff0ed648d9244fde5549efbca155cd81
TheoGandon <theo.gandon9@gmail.com>
2024-03-08 13:36:01 +0100

    V2

--- form.php
+++ form.php
@@ -15,16 +15,32 @@
 
 <?php
   $dest = "uploads/";
-  if ($_FILES['monfichier']['error'] > 0) {
-    $erreur = "Erreur lors du transfert";
-  } else {
-    $resultat = move_uploaded_file($_FILES['monfichier']['tmp_name'], $dest . $_FILES['monfichier']['name']);
-    if ($resultat) {
-      echo "<p class='sendmessage'>Fichier envoyé avec succès !</p>";
+
+  // Vérifie si le fichier est au bon format
+  if (isset($_FILES['monfichier'])) {
+    $file_type = $_FILES['monfichier']['type'];
+    $allowed = array("application/pdf", "image/jpeg", "image/png", "image/gif", "image/jpg", "image/pjpeg");
+    if (!in_array($file_type, $allowed)) {
+      echo "<p class='sendmessage'>Donne des images ou des PDF !!!!</p>";
+  
+  // Vérifie si le fichier est present
+    } else if ($_FILES['monfichier']['error'] > 0) {
+      $erreur = "Erreur lors du transfert";
+
+  // Vérifie si le fichier est trop gros
+    } else if ($_FILES['monfichier']['size'] > 500000) {
+      echo "<p class='sendmessage'>Le fichier est trop gros</p>";
+
+  // Vérifie si le fichier est bien envoyé
     } else {
-      echo "<p class='sendmessage'>Erreur lors du l'envoie du fichier.</p>";
+      $resultat = move_uploaded_file($_FILES['monfichier']['tmp_name'], $dest . $_FILES['monfichier']['name']);
+      if ($resultat) {
+        echo "<p class='sendmessage'>Fichier envoyé avec succès !</p>";
+      } else {
+        echo "<p class='sendmessage'>Erreur lors du l'envoie du fichier.</p>";
+      }
     }
   }
-  ?>
+?>
 </body>
 </html>
